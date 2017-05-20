4bc3cc72e41dff561047d752c0398993764630b7
Joel Veneracion <84veneracionj@gmail.com>
2017-05-20 00:57:38 -0400

    Installed source tree again.  However, the IDE I am using for the project now is Blue J.  This is just a test commit under the IDE

--- RatscrewGUIRunner.java
+++ RatscrewGUIRunner.java
@@ -1,4 +1,5 @@
 /**
+ * A simple commit testing comment
  * This is a class that plays the GUI version of the Elevens game.
  * See accompanying documents for a description of how Elevens is played.
  */
